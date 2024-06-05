22fe0272e9460813092b7744b60738d555adb28b
boogyber14 <cequina30@gmail.com>
2024-06-05 22:59:22 +0800

    Here's my text editor josh.

--- target/debug/build/thiserror-ce95d7d0ca8bcc0d/build_script_build-ce95d7d0ca8bcc0d.d
+++
@@ -1,5 +0,0 @@
-/home/rickrick/RustroverProjects/Text-Editor2/target/debug/build/thiserror-ce95d7d0ca8bcc0d/build_script_build-ce95d7d0ca8bcc0d: /home/rickrick/.cargo/registry/src/index.crates.io-6f17d22bba15001f/thiserror-1.0.61/build.rs
-
-/home/rickrick/RustroverProjects/Text-Editor2/target/debug/build/thiserror-ce95d7d0ca8bcc0d/build_script_build-ce95d7d0ca8bcc0d.d: /home/rickrick/.cargo/registry/src/index.crates.io-6f17d22bba15001f/thiserror-1.0.61/build.rs
-
-/home/rickrick/.cargo/registry/src/index.crates.io-6f17d22bba15001f/thiserror-1.0.61/build.rs:
--- target/debug/deps/unicode_ident-c0856f1346db7e41.d
+++
@@ -1,8 +0,0 @@
-/home/rickrick/RustroverProjects/Text-Editor2/target/debug/deps/libunicode_ident-c0856f1346db7e41.rmeta: /home/rickrick/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.12/src/lib.rs /home/rickrick/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.12/src/tables.rs
-
-/home/rickrick/RustroverProjects/Text-Editor2/target/debug/deps/libunicode_ident-c0856f1346db7e41.rlib: /home/rickrick/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.12/src/lib.rs /home/rickrick/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.12/src/tables.rs
-
-/home/rickrick/RustroverProjects/Text-Editor2/target/debug/deps/unicode_ident-c0856f1346db7e41.d: /home/rickrick/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.12/src/lib.rs /home/rickrick/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.12/src/tables.rs
-
-/home/rickrick/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.12/src/lib.rs:
-/home/rickrick/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.12/src/tables.rs:
